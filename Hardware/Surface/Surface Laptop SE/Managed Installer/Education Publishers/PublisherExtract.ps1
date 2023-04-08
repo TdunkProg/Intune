630ab401edc0628a8fa1776b4f4a4bbfd15b6ec4
Tyler Duncan <tyler.duncan@outlook.com>
2023-04-08 11:59:26 -0500

    Updated WDAC Changes

---
+++ Hardware/Surface/Surface Laptop SE/Managed Installer/Education Publishers/PublisherExtract.ps1
@@ -0,0 +1,8 @@
+﻿<FilePublisherRule Id="647cb1c9-3661-4b53-af1e-9b658ae201f6" Name="MICROSOFT.MANAGEMENT.SERVICES.INTUNEWINDOWSAGENT.EXE version 1.53.202.0 exactly in MICROSOFT INTUNE from O=MICROSOFT CORPORATION, L=REDMOND, S=WASHINGTON, C=US" Description="" UserOrGroupSid="S-1-1-0" Action="Allow">
+			<Conditions>
+				<FilePublisherCondition PublisherName="O=MICROSOFT CORPORATION, L=REDMOND, S=WASHINGTON, C=US" ProductName="*" BinaryName="MICROSOFT.MANAGEMENT.SERVICES.INTUNEWINDOWSAGENT.EXE">
+					<BinaryVersionRange LowSection="*" HighSection="*" />
+				</FilePublisherCondition>
+			</Conditions>
+		</FilePublisherRule>
+	</RuleCollection>
